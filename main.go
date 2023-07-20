f73648243658621f222535855382d24691fcd321
Niranjan <kneerunjun@gmail.com>
2023-07-21 04:43:44 +0530

    now added basic splash page content , ready to add angularjs

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	FVerbose, FLogF bool
+	logFile         string
+)
+
+func init() {
+	/* ======================
+	-verbose=true would mean log.Debug can work
+	-verbose=false would mean log.Debug will be hidden
+	-flog=true: all the log output shall be onto a file
+	-flog=false: all the log output shall be on stdout
+	- We are setting the default log level to be Info level
+	======================= */
+	flag.BoolVar(&FVerbose, "verbose", false, "Level of logging messages are set here")
+	flag.BoolVar(&FLogF, "flog", false, "Direction in which the log should output")
+	// Setting up log configuration for the api
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: false,
+		FullTimestamp: true,
+	})
+	log.SetReportCaller(false)
+	// By default the log output is stdout and the level is info
+	log.SetOutput(os.Stdout)     // FLogF will set it main, but dfault is stdout
+	log.SetLevel(log.DebugLevel) // default level info debug but FVerbose will set it main
+	logFile = os.Getenv("LOGF")
+}
+
+// ServeIndexHtml : will dispatch the index.html page
+func ServeIndexHtml(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{})
+}
+
+func main() {
+	flag.Parse() // command line flags are parsed
+	log.WithFields(log.Fields{
+		"verbose": FVerbose,
+		"flog":    FLogF,
+	}).Info("Log configuration..")
+	if FVerbose {
+		log.SetLevel(log.DebugLevel)
+	}
+	if FLogF {
+		lf, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0664)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"err": err,
+			}).Error("Failed to connect to log file, kindly check the privileges")
+		} else {
+			log.Infof("Check log file for entries @ %s", logFile)
+			log.SetOutput(lf)
+		}
+	}
+	// Loading all environment variables
+	dirStatic := os.Getenv("DIR_STATIC")
+	log.WithFields(log.Fields{
+		"static_dir": dirStatic,
+	}).Debug("echoing static directory")
+	log.Info("Starting server..")
+	defer log.Warn("Server now shutting down..")
+	gin.SetMode(gin.DebugMode)
+	r := gin.Default()
+
+	r.Static("/images", fmt.Sprintf("%s/images/", dirStatic))
+	r.LoadHTMLGlob(fmt.Sprintf("%s/pages/*", dirStatic))
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"app": "aboutme",
+		})
+	})
+	r.GET("/", ServeIndexHtml)
+	log.Fatal(r.Run(":8080"))
+}
